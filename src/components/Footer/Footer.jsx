1097eb7bd13e5b3fbf3dfa5a4339451e4fae6638
Felipe Rodríguez <frodriguez@meatcode.cl>
2020-02-19 16:17:31 -0300

    refactor: Header and Footer components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
   return (
-    <footer className="bg-white">
-      <div className="container flex justify-between max-w-6xl m-auto p-6">
+    <footer className="bg-white text-sm text-center md:text-left">
+      <div className="container flex flex-col md:flex-row justify-between max-w-screen-xl m-auto p-6">
         <p>
           © {new Date().getFullYear()} Komatsu - Todos los derechos reservados
         </p>
-        <p>Contáctanos contacto@ejemplo.com</p>
+        <p>
+          <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
+          Contáctanos{' '}
+          <a href="mailto:contacto@ejemplo.com" className="mx-2 underline">
+            contacto@ejemplo.com
+          </a>
+        </p>
       </div>
     </footer>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Proptypes from 'prop-types';
 import Button from '../commons/Button/Button';
 import Logo from '../commons/Logo/Logo';
+import { layoutGenerator } from 'react-break';
+import { faChevronDown, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const layout = layoutGenerator({
+  mobile: 0,
+  phablet: 550,
+  tablet: 768,
+  desktop: 1024
+});
+
+const OnMobile = layout.is('mobile');
+const OnAtLeastTablet = layout.isAtLeast('tablet');
+const OnAtMostPhablet = layout.isAtMost('phablet');
+const OnDesktop = layout.is('desktop');
 
 const Header = props => {
   const { name } = props;
+
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const username = () => {
+    const str = name;
+    const matches = str.match(/\b(\w)/g);
+    const acronym = matches.join('');
+    return acronym;
+  };
+
   return (
     <header className="bg-white">
-      <div className="container flex justify-between items-center max-w-6xl m-auto p-6">
+      <div className="container flex justify-between items-center max-w-screen-xl m-auto p-6">
         <div className="flex items-center">
           <Logo />
-          <Button>Equipos</Button>
-          <Button>Repuestos</Button>
+          <OnAtLeastTablet>
+            <Button>Equipos</Button>
+            <Button>Repuestos</Button>
+          </OnAtLeastTablet>
         </div>
-        <div className="profile text-sm">{name}</div>
+        <div className="profile flex items-center">
+          <div className="profile__avatar flex items-center justify-center bg-yellow-600 uppercase text-white rounded-full w-10 h-10">
+            {username()}
+          </div>
+          <div
+            className="relative text-sm hover:text-blue-500 py-3 cursor-pointer"
+            onMouseEnter={handleToggle}
+            onMouseLeave={handleToggle}
+          >
+            <span className="mx-4">{name}</span>
+            <FontAwesomeIcon icon={faChevronDown} className="text-yellow-600" />
+            <div
+              className={`profile transition duration-300 ease-in-out px-4 py-6 rounded-lg bg-white absolute z-50 right-0 bottom-0 w-full transform translate-y-full shadow-md ${
+                toggle ? 'visible' : 'invisible'
+              }`}
+            >
+              Cerrar sesión{' '}
+              <FontAwesomeIcon
+                icon={faSignOutAlt}
+                className="text-yellow-600 ml-2"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
