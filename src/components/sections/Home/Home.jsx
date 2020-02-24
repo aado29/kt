82126833956701570f3cb1a0a076631a82a9798f
Felipe Rodríguez <frodriguez@meatcode.cl>
2020-02-24 13:01:45 -0300

    chore: rename component folder

--- src/components/sections/Home/Home.jsx
+++ src/components/sections/Home/Home.jsx
@@ -3,8 +3,8 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
-import BgParts from '../../../assets/images/bg-equipment.png';
-import BgEquipment from '../../../assets/images/bg-parts.png';
+import bgParts from '../../../assets/images/bg-equipment.png';
+import bgEquipment from '../../../assets/images/bg-parts.png';
 import Card from '../../commons/Card/Card';
 import CardTitle from '../../commons/CardTitle/CardTitle';
 
@@ -17,7 +17,7 @@ const Home = () => {
   return (
     <section className="container flex flex-col md:flex-row max-w-6xl mx-auto md:py-20">
       <div className="md:w-1/2">
-        <Card cardBg={BgEquipment}>
+        <Card cardBg={bgEquipment}>
           <CardTitle>Tracking Equipos</CardTitle>
           <p className="card__description text-sm mb-8">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aut
@@ -26,7 +26,7 @@ const Home = () => {
             Eveniet, magni? Hic, dicta?
           </p>
           <Link
-            to="#"
+            to="/equipos"
             className="card__button transition duration-300 ease py-4 lg:px-16 text-sm uppercase font-bold rounded-full lg:border border-solid border-white"
           >
             Ver más{' '}
@@ -35,7 +35,7 @@ const Home = () => {
         </Card>
       </div>
       <div className="md:w-1/2">
-        <Card cardBg={BgParts}>
+        <Card cardBg={bgParts}>
           <CardTitle>Tracking Repuestos</CardTitle>
           <p className="card__description text-sm mb-8">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aut
--- src/views/home/HomeView.jsx
+++ src/views/home/HomeView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import Home from '../../components/modules/Home/Home';
+import Home from '../../components/sections/Home/Home';
 
 const HomeView = () => {
   return (
