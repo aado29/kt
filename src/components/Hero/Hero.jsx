25ccae911ad73ddcd9daf053c0b1cb3145986bb2
Felipe Rodríguez <frodriguez@meatcode.cl>
2020-03-02 16:43:31 -0300

    chore: change hero ipad layout to mobile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,12 +12,12 @@ const Hero = props => {
       className="hero bg-cover"
       style={{ backgroundImage: `url(${bgHero})` }}
     >
-      <div className="hero__container container flex flex-col md:flex-row max-w-screen-xl mx-auto p-6 md:py-10">
-        <div className="md:w-1/2 md:pr-4">
+      <div className="hero__container container flex flex-col lg:flex-row max-w-screen-xl mx-auto p-6 lg:py-10">
+        <div className="lg:w-1/2 lg:pr-4">
           <CardTitle>{title}</CardTitle>
           <p className="text-sm mb-8">{description}</p>
         </div>
-        <div className="md:w-1/2 md:pl-4 flex items-center justify-center">
+        <div className="lg:w-1/2 lg:pl-4 md:flex items-center justify-center whitespace-no-wrap overflow-x-scroll">
           {logos.map((e, i) => {
             return <Logotype key={i} img={e.logo} />;
           })}
--- src/components/commons/Logotype/Logotype.jsx
+++ src/components/commons/Logotype/Logotype.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Logotype = props => {
   return (
-    <div className="bg-white flex items-center justify-center h-16 w-32 rounded inline-flex m-2 px-2">
-      <img src={props.img} alt="Logo" />
+    <div className="bg-white md:flex items-center justify-center h-16 w-32 rounded inline-block md:inline-flex m-2 px-2 text-center">
+      <img src={props.img} alt="Logo" className="mx-auto" />
     </div>
   );
 };
