8c48d12b3252d107515443a6481b8bc8111b99c2
Felipe Rodríguez <frodriguez@meatcode.cl>
2020-03-03 11:35:31 -0300

    feat(Login): initial Login step

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from 'components/Footer/Footer';
+import LoginView from './views/login/LoginView';
 import HomeView from './views/home/HomeView';
 import EquipmentView from './views/equipment/EquipmentView';
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={HomeView} />
+        <Route path="/login" component={LoginView} />
         <Route path="/equipos" component={EquipmentView} />
         <Footer />
       </div>
---
+++ src/components/sections/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from 'components/Form/Form';
+
+const Login = () => {
+  return (
+    <section className="container flex flex-col md:flex-row justify-center items-center max-w-6xl mx-auto md:py-20">
+      <Form />
+    </section>
+  );
+};
+
+export default Login;
